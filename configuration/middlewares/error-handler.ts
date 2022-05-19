065d15d5a0ce1d302c96744dbc865ad5291dfca8
herberts <herberts.fortuna@sanar.com>
2022-05-18 23:42:37 -0300

    feat: add standard-version and husky support

--- configuration/middlewares/error-handler.ts
+++ configuration/middlewares/error-handler.ts
@@ -2,11 +2,12 @@ import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 
 export function errorHandler(): ErrorRequestHandler {
     /* eslint-disable @typescript-eslint/no-unused-vars */
-    return (((err, req: Request, res: Response, next: NextFunction) => {
-        console.error(typeof err);
-        res.status(err.status || 500).json({
-            message: err.message,
-            errors: err.errors,
-        });
-    }) as ErrorRequestHandler)
+    return <ErrorRequestHandler>function (err, req: Request, res: Response, next: NextFunction) {
+        res.status(err.status ?? 500)
+        res.json({
+            success: false,
+            error: err.message
+        })
+        next(err)
+    }
 }
